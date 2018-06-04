b6174a0027de1ba90ba80e0f62d50740e0679be8
DeepInEvil <deba.kgec@gmail.com>
2018-06-04 18:40:22 +0200

    vanilla LM

--- model_lstm.py
+++ model_lstm.py
@@ -7,10 +7,11 @@ from locked_dropout import LockedDropout
 class RNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
 
-    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, tie_weights=False):
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, dropouth=0.5, tie_weights=False):
         super(RNNModel, self).__init__()
         self.drop = nn.Dropout(dropout)
         self.lockdrop = LockedDropout()
+        self.dropouth = dropouth
         self.encoder = nn.Embedding(ntoken, ninp)
         assert rnn_type in ['LSTM', 'QRNN', 'GRU'], 'RNN type is not supported'
         if rnn_type == 'LSTM':
