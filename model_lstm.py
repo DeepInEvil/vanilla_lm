552d6bcba2cd32d07581ffa675ed81b0baffdba5
DeepInEvil <deba.kgec@gmail.com>
2018-06-04 17:33:01 +0200

    vanilla LM

---
+++ model_lstm.py
@@ -0,0 +1,85 @@
+import torch.nn as nn
+import torch
+from torch.autograd import Variable
+from LSTM_topic import LSTMCell
+
+class RNNModel(nn.Module):
+    """Container module with an encoder, a recurrent module, and a decoder."""
+
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, tie_weights=False):
+        super(RNNModel, self).__init__()
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        assert rnn_type in ['LSTM', 'QRNN', 'GRU'], 'RNN type is not supported'
+        if rnn_type == 'LSTM':
+            self.rnns = [
+                LSTMCell(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else (ninp if tie_weights else nhid),
+                         bias=True) for l in range(nlayers)]
+        else:
+            try:
+                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]
+            except KeyError:
+                raise ValueError( """An invalid option for `--model` was supplied,
+                                 options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
+            self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity=nonlinearity, dropout=dropout)
+        self.decoder = nn.Linear(nhid, ntoken)
+
+        # Optionally tie weights as in:
+        # "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
+        # https://arxiv.org/abs/1608.05859
+        # and
+        # "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
+        # https://arxiv.org/abs/1611.01462
+        if tie_weights:
+            if nhid != ninp:
+                raise ValueError('When using the tied flag, nhid must be equal to emsize')
+            self.decoder.weight = self.encoder.weight
+
+        self.init_weights()
+
+        self.rnn_type = rnn_type
+        self.nhid = nhid
+        self.nlayers = nlayers
+
+    def run_lstmcell(self, rnnmodel, input, hdn):
+        hx, cx = hdn
+        hx_all = []
+        cx_all = []
+        for j in range(input.size(0)):
+            hx, cx = rnnmodel(input[j], (hx, cx))
+            hx_all.append(hx)
+            cx_all.append(cx)
+        return torch.stack(hx_all), torch.stack(cx_all)
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, hidden):
+        emb = self.drop(self.encoder(input))
+        #output, hidden = self.rnn(emb, hidden)
+        new_hidden = []
+        #raw_output, hidden = self.rnn(emb, hidden)
+        raw_outputs = []
+        outputs = []
+        for l, rnn in enumerate(self.rnns):
+            raw_output, new_h = self.run_lstmcell(rnn, emb, hidden[l])
+            new_hidden.append(new_h)
+            raw_outputs.append(raw_output)
+            if l != self.nlayers - 1:
+                #self.hdrop(raw_output)
+                raw_output = self.lockdrop(raw_output, self.dropouth)
+                outputs.append(raw_output)
+        output = self.drop(outputs)
+        decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
+        return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters())
+        if self.rnn_type == 'LSTM':
+            return (Variable(torch.zeros(self.nlayers, bsz, self.nhid)).cuda(),
+                    Variable(torch.zeros(self.nlayers, bsz, self.nhid)).cuda())
+        else:
+            return Variable(torch.zeros(self.nlayers, bsz, self.nhid).cuda())
