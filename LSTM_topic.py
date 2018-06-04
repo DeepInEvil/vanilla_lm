075ccade7e86c882724823a78f13be3482dd2c81
DeepInEvil <deba.kgec@gmail.com>
2018-06-04 17:37:10 +0200

    vanilla LM

--- LSTM_topic.py
+++ LSTM_topic.py
@@ -34,12 +34,8 @@ def LSTMCell_func(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None, topic=None, t
     gates = F.linear(input, w_ih, b_ih) + F.linear(hx, w_hh, b_hh)
 
     ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
-    #print topic.size(), topic_i_w.size(), ingate.size()
-    #ingate = F.sigmoid(ingate + F.linear(topic, topic_i_w))
-    #print ingate.size()
     ingate = F.sigmoid(ingate)
 
-    #forgetgate = F.sigmoid(forgetgate + F.linear(topic, topic_f_w))
     forgetgate = F.sigmoid(forgetgate)
     cellgate = F.tanh(cellgate)
     outgate = F.sigmoid(outgate)
