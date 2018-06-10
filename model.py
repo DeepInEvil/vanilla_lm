8c7fa49c0206ef31da72c9f0f753e3f6713cc0ee
DeepInEvil <deba.kgec@gmail.com>
2018-06-10 13:58:06 +0200

    vanilla LM

--- model.py
+++ model.py
@@ -51,7 +51,6 @@ class RNNModel(nn.Module):
     def forward(self, input, hidden):
         emb = self.drop(self.encoder(input))
         print (hidden)
-        print (emb.size(), hidden.size())
         output, hidden = self.rnn(emb, hidden)
         output = self.drop(output)
         decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
